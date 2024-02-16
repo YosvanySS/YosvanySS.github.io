edbfe28078f0686ec4f6406cdb586b572bc6d6c0
YosvanySS <113038567+YosvanySS@users.noreply.github.com>
2024-02-16 16:22:43 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-  `models/${objToRender}/scene.gltf`,
+  `models/pfu/scene.gltf`,
   function (gltf) {
     //If the file is loaded, add it to the scene
     object = gltf.scene;
